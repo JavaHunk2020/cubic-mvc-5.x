d618968ded9dadf087628fdb2c9cca890c89a548
JavaHunk2020 <javahunk2020@gmail.com>
2021-02-20 01:03:32 +0530

    testing git magic!

---
+++ src/main/java/com/cubicit/controller/Kuebiko.java
@@ -0,0 +1,13 @@
+package com.cubicit.controller;
+
+/**
+ * 
+ * @author javahunk
+ *
+ */
+public class Kuebiko {
+
+	public static void main(String[] args) {
+		System.out.println("Magic of git!");
+	}
+}
